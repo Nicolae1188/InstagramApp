a84fc476bf3fb8689dfc09b0592d258687ecad25
BruceBelcher <bruce.belcher@hotmail.co.uk>
2019-06-26 15:09:48 +0100

    Package lock re added

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Post from './components/Post';
+import ImageInCircle from './components/ImageInCircle';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Header />
-       <div>
+        <div>
+          <ImageInCircle />
+        </div>
+        <div>
           <Post />
-         </div>
+        </div>
       </div>
     );
   }
