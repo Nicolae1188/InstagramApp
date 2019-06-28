a1a03f7023a1965925a926d3880dc0eb0d2b326f
BruceBelcher <bruce.belcher@hotmail.co.uk>
2019-06-28 10:27:26 +0100

    interim save

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -3,22 +3,25 @@ import Header from './components/Header';
 import Post from './components/Post';
 import Footer from './components/Footer'
 import ImageInCircle from './components/ImageInCircle';
+import "./App.css";
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <Header />
+      <div className="wholeScreen">
         <div>
-          <ImageInCircle />
-        </div>
-        <div>
-          <Post />
-        <div>
-        <Footer />
-           </div>
-         </div>
-        </div>      
+          <Header />
+          <div>
+            <ImageInCircle />
+          </div>
+          <div>
+            <Post />
+            <div>
+              <Footer />
+            </div>
+          </div>
+        </div>   
+      </div> //end wholeScreen
     );
   }
 }
--- instagram-app/src/components/ImageInCircle/index.js
+++ instagram-app/src/components/ImageInCircle/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./ImageInCircle.css";
+import "../../App.css";
 import { tsPropertySignature } from '@babel/types';
 
 class ImageInCircle extends React.Component {
@@ -7,32 +8,36 @@ class ImageInCircle extends React.Component {
         storyImage: "https://scontent-lht6-1.cdninstagram.com/vp/e2c94cacf60c6511b2cbe1a99999eb28/5DA1F964/t51.2885-19/s320x320/14596696_1806049626330886_8739327968465125376_a.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com",
         storyName: "Max"
     }
+
     render(){
         return (
             <div className="StoriesBar">
-                {/* <div className="StoriesBox" > */}
+                    <DisplayStoryCard name={this.state.storyName} image={this.state.storyImage} story={this.displayStory} />
                     <DisplayStoryCard name={this.state.storyName} image={this.state.storyImage} />
                     <DisplayStoryCard name={this.state.storyName} image={this.state.storyImage} />
                     <DisplayStoryCard name={this.state.storyName} image={this.state.storyImage} />
                     <DisplayStoryCard name={this.state.storyName} image={this.state.storyImage} />
                     <DisplayStoryCard name={this.state.storyName} image={this.state.storyImage} />
                     <DisplayStoryCard name={this.state.storyName} image={this.state.storyImage} />
-                    <DisplayStoryCard name={this.state.storyName} image={this.state.storyImage} />
-
-                {/* </div> */}
             </div>
         );
     }
-}
+} //end ImageInCircle
 
 const DisplayStoryCard = props => (
     <div className="StoriesBox" >
         <div className="Circle">
-            <img src={props.image} alt="" />
+            <img src={props.image} alt="" onClick={props.story} />
         </div>
         <p>{props.name}</p>
     </div>
 )
+
+class openStory extends React.Component {
+
+
+
+} //end openStory
 
 
 export default ImageInCircle;
