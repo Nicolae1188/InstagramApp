d4ea90d1ce435f72e037d5480d17e6a17ce7e032
Your Name <administrator@CN-7.lan>
2019-06-28 16:03:14 +0100

    Marks changes

--- instagram-app/src/components/Header/index.js
+++ instagram-app/src/components/Header/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import "./Header.css";
+import lilMan from "./lilman.png"
+import lilheart from "./lilheart.png"
+import Compass from "./Compass.png"
 
 class Header extends React.Component {
     render(){
@@ -10,6 +13,9 @@ class Header extends React.Component {
                         <a className="Nav-brand-logo" href="/">
                             Instagram
                         </a>
+                        <img src={Compass}></img>
+                        <img src={lilheart}></img>
+                        <img src={lilMan}></img>
                     </div>
                 </div>
             </nav>
